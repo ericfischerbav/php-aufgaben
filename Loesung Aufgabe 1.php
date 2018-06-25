d922a11de47c52025dff45dcf5483ea8425a7348
Eric Fischer <eric.fischer.bav@gmail.com>
2018-06-25 10:01:57 +0200

    Lösung Aufgabe 1

---
+++ Loesung Aufgabe 1.php
@@ -0,0 +1,18 @@
+<?php
+
+	function suche_komponenten($produkt) {
+		$connection = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PW');
+		mysqli_select_db($connection, 'DB_NAME');
+		
+		$sql = "SELECT * FROM komponenten WHERE produkt = ".$produkt;
+		$result = mysqli_query($connection, $sql);
+		$komponenten = array();
+		
+		while($komponente = mysqli_fetch_assoc($result)) {
+			$komponenten[] = $komponente;
+		}
+		
+		return $komponenten;
+	}
+
+?>
